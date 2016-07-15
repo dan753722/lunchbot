f19c14012d0960d1dcd08021949023434dd8c934
Daniel Sun <H.Sun@qsrinternational.com>
2016-07-15 15:44:53 +1000

    lunchbot for slack initial commit

---
+++ lunchbot.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var Botkit = require("botkit");
+var controller = Botkit.slackbot({
+  debug: true
+});
+
+var messageBank = require("./lib/botMessages.js");
+var botScope = ["direct_message", "direct_mention", "mention"];
+
+controller.spawn({
+  token: "YOUR_BOT_TOKEN"
+}).startRTM();
+
+controller.hears("suggest lunch", botScope, messageBank.hanaichi);
+
+controller.hears("[Dd]an", botScope, messageBank.danTheMan);
---
+++ start.bat
@@ -0,0 +1,4 @@
+#npm install botkit --production
+#npm install --save botkit 
+#token=REPLACE_THIS_WITH_YOUR_TOKEN node lunchbot.js
+node lunchbot.js
